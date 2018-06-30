58825a5463ad16822749778d0a0f58f0ab633214
Hayden Betts <haydenbetts@gmail.com>
2018-06-30 20:22:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student|
+      student = self.new(student)
+      self.all << student
+    end
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,6 +20,6 @@ class Student
   end
 
   def self.all
-
+    @@all
   end
 end
